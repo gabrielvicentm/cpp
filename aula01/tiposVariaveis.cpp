271ec835eadafbbc6b52e70cc84bbe4008abd293
gabrielvicentm <gabrielvicentee068@gmail.com>
2026-05-24 21:57:00 -0300

    signed vs unsigned

--- aula01/tiposVariaveis.cpp
+++ aula01/tiposVariaveis.cpp
@@ -1,4 +1,4 @@
-int moedas = 259; // inteiro 'normal'
+int vida = 99; // inteiro 'normal'
 
 float velocidade = 600.0f; // float só tem 1 casa decimal, usado quando nao precisa de precisao extrema, se esquecer o f toma gap
 double velocidadePrecisa = 600.0623664343; // usado quando precisão importa
@@ -6,4 +6,20 @@ double velocidadePrecisa = 600.0623664343; // usado quando precisão importa
 char letrinha = 'W'; //guarda um caractere, aspas simples
 
 bool estaVivo = true; // true or false
+
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+
+// SIGNED VS UNSIGNED
+
+int moedas = -10; //int 'normal' aceita vvalores negativos
+
+unsigned int moedas = 10; //unsigned int NÃO ACEITA VALORES NEGATIVOS
+
+
+
+
+
+
 
