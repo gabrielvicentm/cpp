e161c1352b9725d69ababab16f869e04c9200f98
gabrielvicentm <gabrielvicentee068@gmail.com>
2026-05-24 21:42:10 -0300

    declaracao, definicao e corpo de uam função

---
+++ aula01/funcaoBasica.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int Multiplicar(int a, int b);
+
+int main()
+{
+    std::cout << Multiplicar(3, 4) << "\n";
+    return 0;
+}
+
+int Multiplicar(int a, int b)
+{
+   return a * b;
+}
+
+
