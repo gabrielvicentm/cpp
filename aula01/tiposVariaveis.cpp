d6dedd6498d98fd86dd75caf508d5d9913e144dd
gabrielvicentm <gabrielvicentee068@gmail.com>
2026-05-24 21:53:10 -0300

    tipos básico de variáveis

---
+++ aula01/tiposVariaveis.cpp
@@ -0,0 +1,9 @@
+int moedas = 259; // inteiro 'normal'
+
+float velocidade = 600.0f; // float só tem 1 casa decimal, usado quando nao precisa de precisao extrema, se esquecer o f toma gap
+double velocidadePrecisa = 600.0623664343; // usado quando precisão importa
+
+char letrinha = 'W'; //guarda um caractere, aspas simples
+
+bool estaVivo = true; // true or false
+
